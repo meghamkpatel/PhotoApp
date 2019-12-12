a6f59d02e72440db045a5016157aff6a0dfc75ee
Megha Patel <mpatel43@binghamton.edu>
2019-12-12 11:30:07 -0500

    added permission

--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -2,6 +2,10 @@ package com.example.photoeditor;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.Manifest;
+import android.annotation.SuppressLint;
+import android.content.pm.PackageManager;
+import android.os.Build;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +14,31 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private static final String[] PERMISSIONS = {
+        Manifest.permission.CAMERA
+    };
+    private static final int REQUEST_PERMISSIONS = 34;
+    private static final int PERMISSIONS_COUNT = 1;
+    @SuppressLint("NewApi")
+    private boolean arePermissionsDenied(){
+        for(int i = 0; i < PERMISSIONS_COUNT; i++){
+            if(checkSelfPermission(PERMISSIONS[i])!= PackageManager.PERMISSION_GRANTED){
+                return true;
+            }
+        }
+        return false;
+    }
+    public void onRequestPermissionResult(int requestCode, String[] permiissions, int[] grantedResults){
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+    }
+    @Override
+    protected void onResume(){
+        super.onResume();
+        if(Build.VERSION.SDK_INT >= Build.VERSION_CODES.M && arePermissionsDenied()){
+            requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
+            return;
+        }
     }
 }
