be18f8cd8882ac2056aca0e6f78ba750ab3de9bb
Megha Patel <mpatel43@binghamton.edu>
2019-12-14 20:21:52 -0500

    working

--- app/src/main/java/com/example/photoeditor/MainActivity.java
+++ app/src/main/java/com/example/photoeditor/MainActivity.java
@@ -4,9 +4,17 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.Manifest;
 import android.annotation.SuppressLint;
+import android.app.ActivityManager;
+import android.content.Context;
 import android.content.pm.PackageManager;
+import android.graphics.Camera;
 import android.os.Build;
 import android.os.Bundle;
+import android.view.SurfaceHolder;
+import android.view.SurfaceView;
+import android.widget.FrameLayout;
+
+import java.io.IOException;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -30,9 +38,23 @@ public class MainActivity extends AppCompatActivity {
         }
         return false;
     }
-    public void onRequestPermissionResult(int requestCode, String[] permiissions, int[] grantedResults){
+    @SuppressLint("NewApi")
+    public void onRequestPermissionResult(int requestCode, String[] permissions, int[] grantResults){
         super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        if(requestCode == REQUEST_PERMISSIONS && grantResults.length > 0){
+            if(arePermissionsDenied()){
+                ((ActivityManager) (this.getSystemService(ACTIVITY_SERVICE))).clearApplicationUserData();
+                recreate();
+            }else{
+                onResume();
+            }
+        }
     }
+    private boolean isCameraInitialized;
+    private Camera mCamera = null;
+    private static SurfaceHolder myHolder;
+    private static CameraPreview mPreview;
+    private FrameLayout preview;
     @Override
     protected void onResume(){
         super.onResume();
@@ -40,5 +62,57 @@ public class MainActivity extends AppCompatActivity {
             requestPermissions(PERMISSIONS, REQUEST_PERMISSIONS);
             return;
         }
+        if(isCameraInitialized){
+            mPreview = new CameraPreview(this, mCamera);
+            preview = findViewById(R.id.camera);
+            preview.addView(mPreview);
+        }
+    }
+    private static int rotation;
+    private void rotateCamera(){
+        if(mCamera != null){
+            rotation = this.getWindowManager().getDefaultDisplay().getRotation();
+            if(rotation ==0){
+                rotation = 90;
+            }else if(rotation == 1){
+                rotation = 0;
+            }else if(rotation == 2){
+                rotation = 270;
+            }else{
+                rotation = 180;
+            }
+        }
+    }
+    private class CameraPreview extends SurfaceView implements SurfaceHolder.Callback{
+        private static SurfaceHolder mHolder;
+        private static Camera mCamera;
+
+        private CameraPreview(Context context, Camera camera){
+            super(context);
+            mCamera = camera;
+            mHolder = getHolder();
+            mHolder.addCallback(this);
+        }
+        // surfaceView class
+        public void surfaceCreated(SurfaceHolder holder){
+            myHolder = holder;
+            try{
+                mCamera.setPreviewDisplay(holder);
+                mCamera.startPreview();
+            }catch(IOException e){
+                e.printStackTrace();
+            }
+        }
+
+        @Override
+        public void surfaceChanged(SurfaceHolder surfaceHolder, int i, int i1, int i2) {
+
+        }
+
+        @Override
+        public void surfaceDestroyed(SurfaceHolder surfaceHolder) {
+
+        }
+
     }
 }
